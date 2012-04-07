067a24ccbf0ded32490b7bf6eefba5b8ccdc59e4
anton whalley <antonwhalley@number9systems.com>
2012-04-07 20:09:28 +0100

    Console outs for test

--- test/test-mongodb.js
+++ test/test-mongodb.js
@@ -10,15 +10,19 @@ var client = new Db('test', new Server("127.0.0.1", 27017, {})),
       collection.insert({a:2}, function(err, docs) {
 
         collection.count(function(err, count) {
+
             count.should.equal(1);
+            console.log('Count should be one');
         });
 
           console.log('Find Test');
         // Locate all the entries using find
         collection.find().toArray(function(err, results) {
           results.length.should.equal(1); 
+          console.log('length should be one');
           //test.assertEquals(1, results.length);
           results[0].a.should.equal(2);
+          console.log('Value should be two');
           ///test.assertTrue(results[0].a === 2);
           collection.drop(function(err, collection) {
                   db.close();
