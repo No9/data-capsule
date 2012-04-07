1dd07cf835ac8dd9960daff9c7ce9adf5bc7471d
anton whalley <antonwhalley@number9systems.com>
2012-04-07 13:50:08 +0100

    baseline mongo connection test

---
+++ test/test-mongodb.js
@@ -0,0 +1,34 @@
+var Db = require('mongodb').Db,
+  Connection = require('mongodb').Connection,
+  Server = require('mongodb').Server,
+  should = require('should');
+
+var client = new Db('test', new Server("127.0.0.1", 27017, {})),
+    test = function (err, collection) {
+      
+      console.log('Insert Test');
+      collection.insert({a:2}, function(err, docs) {
+
+        collection.count(function(err, count) {
+            count.should.equal(1);
+        });
+
+          console.log('Find Test');
+        // Locate all the entries using find
+        collection.find().toArray(function(err, results) {
+          results.length.should.equal(1); 
+          //test.assertEquals(1, results.length);
+          results[0].a.should.equal(2);
+          ///test.assertTrue(results[0].a === 2);
+          collection.drop(function(err, collection) {
+                  db.close();
+                });  
+          // Let's close the db
+          client.close();
+        });
+      });
+    };
+
+client.open(function(err, p_client) {
+  client.collection('test_insert', test);
+});
